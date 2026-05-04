e94eb0051501ba80bd575a3d6ca72cfa66d29fc9
Thuy Nguyen <luongthts2302011@fpt.edu.vn>
2026-05-04 22:12:36 +0700

    resolve cors config conflict

--- Index_Electric_server/src/main/java/com/index_electric_server/config/CorsConfig.java
+++ Index_Electric_server/src/main/java/com/index_electric_server/config/CorsConfig.java
@@ -19,8 +19,7 @@ public class CorsConfig {
                                 "http://localhost",
                                 "http://127.0.0.1",
                                 "http://localhost:5174",
-                                "http://localhost:5173",
-                                "http://localhost:8080"
+                                "http://localhost:5173"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*")
